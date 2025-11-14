56d0496c4545e24ef346f3f53d08a0b0cabf610b
Ashish Mali <ashishmali456@gmail.com>
2025-11-14 19:07:18 +0530

    Time: 7 ms (57.88%), Space: 18.7 MB (55.11%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -16,3 +16,17 @@ class Solution:
 
 # Total Sum = left_sum + nums[i] + right_sum
 # Total Sum - left_sum - nums[i] = right
+
+# Time Complexity O(n) and space O(1) 
+# Another approach
+# total = sum(nums) # use of in built function
+#         left = 0
+
+#         for i, num in enumerate(nums):
+#             # right = total - left - num
+#             # pivot condition: left == right  <=>  2*left + num == total
+#             if 2 * left + num == total:
+#                 return i
+#             left += num
+
+#         return -1
