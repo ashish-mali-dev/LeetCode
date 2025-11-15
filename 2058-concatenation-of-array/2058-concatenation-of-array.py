6c53d50596f1d091bcbd63717efed13c9bf59c98
Ashish Mali <ashishmali456@gmail.com>
2025-11-15 18:27:32 +0530

    Time: 0 ms (100%), Space: 17.8 MB (92.08%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -3,6 +3,5 @@ class Solution:
         ans = [0]*len(nums)*2
         for i in range(len(nums)):
             ans[i] = nums[i]
-            print(len(nums)//2+i)
             ans[len(nums)+i] = nums[i]
         return ans
