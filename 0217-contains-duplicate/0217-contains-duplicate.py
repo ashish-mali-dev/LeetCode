14d4a4aefd743cb8d6940bb73628770785af7164
Ashish Mali <ashishmali456@gmail.com>
2025-12-23 18:46:54 +0530

    Time: 19 ms (59.31%), Space: 26 MB (19.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: bool
         """
         distinct_values = set()
-    
+
         for num in nums:
             if num in distinct_values:
                 return True
             distinct_values.add(num)
-
         return False
