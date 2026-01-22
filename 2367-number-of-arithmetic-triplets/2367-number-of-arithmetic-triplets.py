d1ceea0e22cafd90746806af62c322437528a0ec
Ashish Mali <ashishmali456@gmail.com>
2026-01-22 22:38:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (26.57%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -7,3 +7,9 @@ class Solution:
             if(num+diff) in check and (num+(2*diff)) in check:
                 res+=1
         return res
+
+#i < j < k,
+#nums[j] - nums[i] == diff means nums[j] = diff + nums[i]
+#nums[k] - nums[j] == diff replace above in thus nums[k] - (diff + nums[i])= diff
+# i.e. nums[k] = diff+diff+nums[i] ~ nums[k] = nums[i]+  2* diff
+# if we get correct num then we can check for j,k inclusion
