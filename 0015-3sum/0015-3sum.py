567b7836194b4a7f4c6ca40e5eea706e38a6af02
Ashish Mali <ashishmali456@gmail.com>
2026-02-08 19:05:33 +0530

    Time: 605 ms (54.5%), Space: 22.3 MB (30.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,43 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res=[]
         nums.sort()
-        n = len(nums)
-        res = []
 
-        for i in range(n):
-            # Early stop
-            if nums[i] > 0:
-                break
-
-            # Skip duplicate i
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+            
+            j=i+1
+            k=len(nums)-1
 
-            low = i + 1
-            high = n - 1
-            target = -nums[i]
+            while j<k:
+                total = nums[i] + nums[j]+ nums[k]
 
-            while low < high:
-                s = nums[low] + nums[high]
-
-                if s == target:
-                    res.append([nums[i], nums[low], nums[high]])
-
-                    # Move both pointers
-                    low += 1
-                    high -= 1
-
-                    # Skip duplicate low
-                    while low < high and nums[low] == nums[low - 1]:
-                        low += 1
-
-                    # Skip duplicate high
-                    while low < high and nums[high] == nums[high + 1]:
-                        high -= 1
-
-                elif s < target:
-                    low += 1
+                if total>0:
+                    k=k-1
+                elif total<0:
+                    j+=1
                 else:
-                    high -= 1
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
 
+                    while nums[j] == nums[j-1 ] and j<k:
+                        j+=1
         return res
