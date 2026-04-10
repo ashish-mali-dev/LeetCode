89c7e9236338b4ebab08b993e79c1fbe6c424aa9
Ashish Mali <ashishmali456@gmail.com>
2026-04-10 22:32:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (8.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0  # index for placing non-val elements
+        low=0
 
         for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-
-        return k
+            if nums[i]!=val:
+                nums[low]=nums[i]
+                low+=1
+        return low
