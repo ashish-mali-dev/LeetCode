055075a7d88db43c352ff07af8aa36758dbd078a
Ashish Mali <ashishmali456@gmail.com>
2026-07-09 19:58:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -5,10 +5,10 @@ class Solution:
         elif(n<10):
             return False
         else:
-            sum =0
+            total =0
             while(n>0):
                 temp = n%10
-                sum += temp*temp
+                total += temp*temp
                 n= n//10
-            return self.isHappy(sum) 
+            return self.isHappy(total) 
         
